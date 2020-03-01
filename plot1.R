31e7ad10cd1c17e008d66eaaf9c4072755cfaf67
Husseinawala <61643125+Husseinawala@users.noreply.github.com>
2020-03-01 12:22:53 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -12,7 +12,7 @@ Global_active_power <- as.numeric(power$Global_active_power)
 
 par(mfrow=c(1,1))
 
-hist(Global_active_power,col='red',xlab="Global Active Power (Kilowatt)")
+hist(Global_active_power,col='red',main= "Global Active Power", xlab="Global Active Power (Kilowatt)")
 
 dev.copy(png,"Plot1.png",width = 480, height = 480, units = "px")
 
